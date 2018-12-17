bfe07bc29037a2710b5d1d794b51c1ef324a9b24
Paul Kim <9257383+paulckim@users.noreply.github.com>
2018-12-17 00:29:11 -0800

    Transparent Navbar and Sidenav (#2)
    * finalize swiper as carousel framework
    
    * remove materialize-css & add custom bullet css
    
    * add transparent nav
    
    * add responsive image css
    
    * add text highlight color
    
    * finalize transparent navs
    
    * use latest materialize-css

--- src/App.js
+++ src/App.js
@@ -3,59 +3,108 @@ import Swiper from "swiper";
 import './index.css';
 
 // TODO: remove hardcoded urls and grab from CDN
-const url1 = "https://newevolutiondesigns.com/images/freebies/polygon-wallpaper-7.jpg";
-const backgroundImg1 = {
-  backgroundImage: `url(${url1})`
-}
+const url1 = "https://i.imgur.com/jdBlxYT.jpg";
+const backgroundImg1 = { backgroundImage: `url(${url1})` };
 
-const url2 = "https://wallpaperaccess.com/full/692106.jpg";
-const backgroundImg2 = {
-  backgroundImage: `url(${url2})`
-}
+const url2 = "https://archive-media-1.nyafuu.org/wg/image/1409/00/1409002784203.png";
+const backgroundImg2 = { backgroundImage: `url(${url2})` };
 
-const url3 = "https://www.desktopbackground.org/download/1600x900/2011/08/12/248957_polygon-wide-hd-desktop-backgrounds-wallpapers-dreamlovewallpapers_1920x1080_h.jpg";
-const backgroundImg3 = {
-  backgroundImage: `url(${url3})`
-}
+const url3 = "https://orig00.deviantart.net/08d8/f/2015/303/b/1/everest_polyscape_by_kermitnirmit-d9eyroo.jpg";
+const backgroundImg3 = { backgroundImage: `url(${url3})` };
+
+const url4 = "https://i.imgur.com/ZmU1Hro.png";
+const backgroundImg4 = { backgroundImage: `url(${url4})` };
+
+const url5 = "https://archive-media-1.nyafuu.org/wg/image/1424/36/1424366309375.png";
+const backgroundImg5 = { backgroundImage: `url(${url5})` };
+
+const url6 = "https://downloadwallpaper.org/wp-content/uploads/2017/08/First-polyscape-using-Photoshop-1920x1080-Need-iPhone-S-Plus-Background-for-IPho-wallpaper-wpc5804805.jpg";
+const backgroundImg6 = { backgroundImage: `url(${url6})` };
+
+const url7 = "https://i.imgur.com/mgJGs64.png";
+const backgroundImg7 = { backgroundImage: `url(${url7})` };
 
 class App extends Component {
   componentDidMount() {
     this._isMounted = true;
+
+    const navElems = window.document.querySelectorAll('.sidenav');
+    this._navInst = window.M.Sidenav.init(navElems);
+
     this._swiperInst = new Swiper('.swiper-container', {
       direction: "vertical",
       mousewheel: true,
-      speed: 600,
+      speed: 800,
       slidesPerView: 1,
-      renderBullet: () => {
-        return '<a class="btn disabled">Button</a>';
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        bulletElement: "a",
-        bulletClass: "btn",
-        bulletActiveClass: "active"
-      }
+      renderBullet: () => '<a className="bullet disabled" />',
+      bulletClass: "bullet",
+      bulletActiveClass: "active"
     });
   }
 
   componentWilUnmount() {
-    if(this._isMounted) this._swiperInst = null;// this._carouselElems.destroy();
+    if(!this._isMounted) return;
+    this._swiperInst.destroy();
+    this._navInst.destroy();
     this._isMounted = false;
   }
 
   render() {
     return (
-      <div className="swiper-container">
-        <div className="swiper-wrapper">
-          <div style={backgroundImg1} className="swiper-slide">
+      <div>
+
+        <div className="swiper-container">
+          <div className="swiper-wrapper">
+            <div style={backgroundImg1} className="swiper-slide autoscale-img">
+              <div className="faded-layer-b">
+              </div>
+            </div>
+            <div style={backgroundImg2} className="swiper-slide autoscale-img">
+              <div className="faded-layer-b"></div>
+            </div>
+            <div style={backgroundImg3} className="swiper-slide autoscale-img">
+              <div className="faded-layer-b"></div>
+            </div>
+            <div style={backgroundImg4} className="swiper-slide autoscale-img">
+              <div className="faded-layer-b"></div>
+            </div>
+            <div style={backgroundImg5} className="swiper-slide autoscale-img">
+              <div className="faded-layer-b"></div>
+            </div>
+            <div style={backgroundImg6} className="swiper-slide autoscale-img">
+              <div className="faded-layer-b"></div>
+            </div>
+            <div style={backgroundImg7} className="swiper-slide autoscale-img">
+              <div className="faded-layer-b"></div>
+            </div>
           </div>
-          <div style={backgroundImg2} className="swiper-slide">
-          </div>
-          <div style={backgroundImg3} className="swiper-slide">
-          </div>
+          <div className="swiper-pagination" />
         </div>
-        <div className="swiper-pagination" />
+
+        <nav className="floating-nav nav-dark">
+          <div className="nav-wrapper">
+            <a href="#!" className="brand-logo center">Paul Cheor Kim</a>
+            <a href="#!" data-target="mobile-nav" className="sidenav-trigger">
+              <i className="material-icons">menu</i>
+            </a>
+            <ul className="right hide-on-med-and-down">
+              <li><a href="#!">About</a></li>
+              <li><a href="#!">Experience</a></li>
+              <li><a href="#!">Skills</a></li>
+              <li><a href="#!">Services</a></li>
+              <li><a href="#!">Blog</a></li>
+              <li><a href="#!">Contact</a></li>
+            </ul>
+          </div>
+        </nav>
+        <ul className="sidenav sidenav-dark" id="mobile-nav">
+          <li><a href="#!">About</a></li>
+          <li><a href="#!">Experience</a></li>
+          <li><a href="#!">Skills</a></li>
+          <li><a href="#!">Services</a></li>
+          <li><a href="#!">Blog</a></li>
+          <li><a href="#!">Contact</a></li>
+        </ul>
       </div>
     );
   }
@@ -66,17 +115,6 @@ export default App;
 
 
 // LANDSCAPE BACKGROUNDS
-
-//"https://newevolutiondesigns.com/images/freebies/polygon-wallpaper-7.jpg"
-//https://wallpaperaccess.com/full/692106.jpg
-//https://www.desktopbackground.org/download/1600x900/2011/08/12/248957_polygon-wide-hd-desktop-backgrounds-wallpapers-dreamlovewallpapers_1920x1080_h.jpg
-
-
-// PORTRAIT BACKGROUNDS
-
-//https://i.pinimg.com/originals/15/76/4a/15764afb302086f1b17e2f925e2ec1e4.jpg
-//https://media.idownloadblog.com/wp-content/uploads/2015/02/Shattered-Pattern-Wallpaper-iPhone-6-Plus.jpg
-//https://media.idownloadblog.com/wp-content/uploads/2015/02/Geometric-Iceberg-Wallpaper-iPhone-6-Plus.jpeg
-
+// https://images5.alphacoders.com/598/thumb-1920-598602.png
 
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import "swiper/dist/css/swiper.min.css";
 import "swiper/dist/js/swiper.min.js";
 
+import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/js/materialize.min.js";
+
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
